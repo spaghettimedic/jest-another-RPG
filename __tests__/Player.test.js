55d2eddec3cf8e69edeff5bc5678777aec84e527
spaghettimedic <dsteiner247@gmail.com>
2021-12-02 08:45:28 -0500

    added tests and methods for getHealth(), isAlive(), and reduceHealth()

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -11,7 +11,7 @@ test('creates a player object', () => {
   expect(player.health).toEqual(expect.any(Number));
   expect(player.strength).toEqual(expect.any(Number));
   expect(player.agility).toEqual(expect.any(Number));
-  
+
   expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
 
@@ -32,4 +32,29 @@ test('gets inventory from player or returns false', () => {
   player.inventory = [];
 
   expect(player.getInventory()).toEqual(false);
+});
+
+test("gets player's health value", () => {
+  const player = new Player('Dave');
+
+  expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+  const player = new Player('Dave');
+  expect(player.isAlive()).toBeTruthy();
+
+  player.health = 0;
+  expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+  const player = new Player('Dave');
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+  expect(player.health).toBe(0);
 });
